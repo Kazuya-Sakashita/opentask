98009cb5df18d126bc72bafe3919b4d324c2c095
Kazuya-Sakashita <kz0508@gmail.com>
2026-05-29 07:35:10 +0900

    RuboCopによるコードフォーマットを適用

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :me, only: [:show], controller: :me
+      resource :me, only: [ :show ], controller: :me
       resources :todos, param: :public_id, only: %i[index show create update destroy]
     end
   end
