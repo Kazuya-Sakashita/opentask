ae0352dfd2ba88dcdde72e4f57c9e50e44d06aae
Kazuya-Sakashita <kz0508@gmail.com>
2026-05-28 12:01:00 +0900

    Todo APIのCRUDを実装

--- backend/app/controllers/api/v1/todos_controller.rb
+++ backend/app/controllers/api/v1/todos_controller.rb
@@ -1,2 +1,55 @@
 class Api::V1::TodosController < ApplicationController
+  before_action :set_todo, only: %i[show update destroy]
+
+  def index
+    todos = policy_scope(Todo)
+
+    render json: todos
+  end
+
+  def show
+    authorize @todo
+
+    render json: @todo
+  end
+
+  def create
+    todo = current_user.todos.build(todo_params)
+
+    authorize todo
+
+    if todo.save
+      render json: todo, status: :created
+    else
+      render json: { errors: todo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    authorize @todo
+
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: { errors: @todo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize @todo
+
+    @todo.soft_delete!
+
+    head :no_content
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.active.find_by!(public_id: params[:public_id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :completed)
+  end
 end
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  include Pundit::Authorization
+
+  private
+
+  def current_user
+    User.first
+  end
 end
